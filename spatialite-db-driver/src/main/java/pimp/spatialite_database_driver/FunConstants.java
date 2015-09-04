37754fea1c252853dc19005a302a05fc3f529f48
Kristina Hager <kristina.hager@gmail.com>
2015-09-04 09:10:20 -0500

    adding example java to class

---
+++ spatialite-db-driver/src/main/java/pimp/spatialite_database_driver/FunConstants.java
@@ -0,0 +1,13 @@
+package pimp.spatialite_database_driver;
+
+/**
+ * Created by kristina on 9/4/15.
+ */
+public class FunConstants {
+
+    private final int FOOBAR = 1;
+
+    public int getFOOBAR() {
+        return FOOBAR;
+    }
+}
