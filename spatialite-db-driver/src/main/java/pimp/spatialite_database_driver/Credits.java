8ac5f12319de5581fdec7ce2cb2f9f67abe2e387
Kristina Hager <kristina.hager@gmail.com>
2015-09-05 18:05:29 -0500

    adding NOTE to build files to highlight key changes

---
+++ spatialite-db-driver/src/main/java/pimp/spatialite_database_driver/Credits.java
@@ -0,0 +1,22 @@
+package pimp.spatialite_database_driver;
+
+import java.lang.String;
+
+/**
+ * Created by kristina on 9/4/15.
+ *
+ * This class exists primarily as an example of using regular android java
+ * in a module
+ *
+ * and it also gives credit where it's due!
+ *
+ */
+public class Credits {
+
+    private final int FOOBAR = 1;
+
+    public String getCredits() {
+        return "Source jsqlite java and shared objects in this module are copied from geopaparazzi (see github)";
+    }
+
+}
